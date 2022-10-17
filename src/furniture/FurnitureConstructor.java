7bccc272cf2e2906f09a6ac623ba3dfade8639ad
VladimirBuleev <vovabuleev@gmail.com>
2022-10-17 16:59:12 +0300

    домашка

--- src/furniture/FurnitureConstructor.java
+++ src/furniture/FurnitureConstructor.java
@@ -2,9 +2,12 @@ package furniture;
 
 public class FurnitureConstructor {
     public static void main(String[] args) {
-        Furniture closet = new Furniture("SKÅNEVIK","Черный","Шкаф");
-        Furniture table = new Furniture("IDÅSEN","Коричневый","Стол");
-        Furniture chair = new Furniture("POÄNG","Бежевый","Стул");
+        TypeOfFurniture typeCloset = TypeOfFurniture.CLOSET;
+        Furniture closet = new Furniture("SKÅNEVIK","Черный", typeCloset.getName());
+        TypeOfFurniture typeTable=TypeOfFurniture.TABLE;
+        Furniture table = new Furniture("IDÅSEN","Коричневый",typeTable.getName());
+        TypeOfFurniture typeChair=TypeOfFurniture.CHAIR;
+        Furniture chair = new Furniture("POÄNG","Бежевый", typeChair.getName());
         Furniture[] furnitures ={closet,table,chair};
         for( Furniture furniture:furnitures){
             furniture.result();
